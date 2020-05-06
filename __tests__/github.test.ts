798798f112aaab2ac1daafb21108428aec0adbc7
CrazyMax <crazy-max@users.noreply.github.com>
2020-05-06 22:17:35 +0200

    Fix test

--- __tests__/github.test.ts
+++ __tests__/github.test.ts
@@ -4,6 +4,6 @@ describe('github', () => {
   it('returns 3.96 GitHub release', async () => {
     const release = await github.getRelease('3.96');
     expect(release).not.toBeNull();
-    expect(release.tag_name).toEqual('v3.96');
+    expect(release?.tag_name).toEqual('v3.96');
   });
 });
