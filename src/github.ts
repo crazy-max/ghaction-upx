88a78e2f11fcda42b559d40d760462c2612ea823
CrazyMax <crazy-max@users.noreply.github.com>
2020-05-07 12:13:15 +0200

    Fix github release tag

--- src/github.ts
+++ src/github.ts
@@ -6,7 +6,7 @@ export interface GitHubRelease {
 }
 
 export const getRelease = async (version: string): Promise<GitHubRelease | null> => {
-  const url: string = `https://github.com/upx/upx/releases/${version !== 'latest' ? `v${version}` : version}`;
+  const url: string = `https://github.com/upx/upx/releases/${version}`;
   const http: httpm.HttpClient = new httpm.HttpClient('ghaction-upx');
   return (await http.getJson<GitHubRelease>(url)).result;
 };
