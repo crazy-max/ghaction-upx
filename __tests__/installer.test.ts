c26e09cbb40d0158ddbfbdff5df0834c1b2501c8
CrazyMax <crazy-max@users.noreply.github.com>
2023-01-29 16:03:29 +0100

    check latest and tagged releases using releases-json

--- __tests__/installer.test.ts
+++ __tests__/installer.test.ts
@@ -2,6 +2,30 @@ import {describe, expect, it} from '@jest/globals';
 import * as fs from 'fs';
 import * as installer from '../src/installer';
 
+describe('getRelease', () => {
+  it('returns latest UPX GitHub release', async () => {
+    const release = await installer.getRelease('latest');
+    expect(release).not.toBeNull();
+    expect(release?.tag_name).not.toEqual('');
+  });
+
+  it('returns v3.95 UPX GitHub release', async () => {
+    const release = await installer.getRelease('v3.95');
+    expect(release).not.toBeNull();
+    expect(release?.id).toEqual(12577195);
+    expect(release?.tag_name).toEqual('v3.95');
+    expect(release?.html_url).toEqual('https://github.com/upx/upx/releases/tag/v3.95');
+  });
+
+  it('unknown release', async () => {
+    await expect(installer.getRelease('foo')).rejects.toThrowError(
+      new Error(
+        'Cannot find UPX release foo in https://raw.githubusercontent.com/crazy-max/ghaction-upx/master/.github/upx-releases.json'
+      )
+    );
+  });
+});
+
 describe('installer', () => {
   it('acquires v3.95 version of UPX', async () => {
     const upx = await installer.getUPX('v3.95');
